aa55e742e96e59007b17628e1c8814570d14f089
fe-22 <72713069+fe-22@users.noreply.github.com>
2023-06-21 17:11:54 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,53 +4,55 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
-import my_product.FirstProduct;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		
-		
-		
+
+		double finalValue = 0;
+		char opcao = 0;
+
 		LocalDateTime dataAtual = LocalDateTime.now();
-		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy  HH:mm:SS");
 		String dataFormatada = dataAtual.format(formato);
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		double precoFinal = 0;
-		char opcao;
-		
+
 		do {
-			
-			System.out.println("Digite o produto...........:");
-			String nome = sc.next();
-			System.out.println("Digite o preço do produto............:");
-			double preco = sc.nextDouble();
-			FirstProduct p = new FirstProduct(nome, preco, 0.1);
-			
-			System.out.println("Nome do produto:" + p.nome);
-			System.out.println("Preço do produto:" + p.preco);
-			System.out.println("Valor com desconto:" + p.precoComDesconto());
-			
-			precoFinal += p.precoComDesconto();
-			
-			System.out.println("Deseja entrar com mais pedido (S/N)?");
-			opcao = sc.next().charAt(0);
-		}while(opcao == 'S'|| opcao == 's');
-		
-		System.out.println("Valor total da compra:" + "R$" + precoFinal);
-		
+			System.out.println("Digite o nome do produto.........................:");
+			String name = sc.nextLine().trim();
+			System.out.println("Digite o preço do produto.........................:");
+			String precoStr = sc.nextLine().trim();
+			double price = Double.parseDouble(precoStr);
+			FirstProduct P = new FirstProduct(name, price, 10);
+
+			System.out.println("Nome do produto:" + P.name);
+			System.out.println("Preço do produto:" + "R$" + P.price);
+			System.out.println("Valor com desconto:" + "R$" + P.discontePrice);
+
+			finalValue += P.discontePrice;
+
+			System.out.println("Deseja entrar com mais pedidos (S/N)?");
+			try {
+
+				opcao = sc.nextLine().charAt(0);
+			} catch (StringIndexOutOfBoundsException e) {
+				
+
+			}
+
+		} while (opcao == 'S' || opcao == 's');
+
+		System.out.println("Valor total da compra:" + "R$" + finalValue);
 		System.out.println("Data da compra:" + dataFormatada);
-		
-		
-			sc.close();
-		
-		
+
+		sc.close();
+
 	}
-	
+
 }
+			
 		
-          
+
+
